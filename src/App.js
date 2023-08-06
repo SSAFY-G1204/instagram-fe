18a90e9de492110940f63d8db436447b14a87c99
JoowonKim <99549623+joowining@users.noreply.github.com>
2023-08-06 21:34:52 +0900

    add basic setting for Explore component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Explore from './components/explore/Explore';
 
 function App() {
   return (
     <div className="App">
-   
+      <Explore/>
     </div>
   );
 }
