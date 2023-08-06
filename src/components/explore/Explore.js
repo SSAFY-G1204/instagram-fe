8b0636598605a0bf21e50dcf74ca9eef5ec808bf
JoowonKim <99549623+joowining@users.noreply.github.com>
2023-08-06 21:47:34 +0900

    basic outline and add react-icon package to package.json

--- src/components/explore/Explore.js
+++ src/components/explore/Explore.js
@@ -3,6 +3,19 @@ import './Explore.css';
 
 export default function Explore() {
   return (
-    <div>Explore</div>
+    <div className="frame">
+      <nav className="sidebar_container">
+        <span>side1</span>
+        <span>side2</span>
+        <span>side3</span>
+        <span>side4</span>
+      </nav>
+      <section className="content_container">
+        <span>content1</span>
+        <span>content2</span>
+        <span>content3</span>
+        <span>content4</span>
+      </section>
+    </div>
   )
 }
