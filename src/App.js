dc26aad3e0117582a3b3ba24615fb99440f110db
SunWoong <lovering1771@naver.com>
2023-08-08 10:53:19 +0900

    side bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import SideBar from './components/Reels/SideBar/SideBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <SideBar />
     </div>
   );
 }
