7289a2903e1984c2f02a74bff2022841a288a0e2
Seokjon Son <tnh3113@naver.com>
2023-08-08 22:56:28 +0900

    로그인 및 회원가입 진행 중

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import JoinPage from './components/JoinPage';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
     <div className="App">
-   
+      <LoginPage/>
     </div>
   );
 }
