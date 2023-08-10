c1ab7a680efe4cf2ebf2f011a61a699050d12d21
JoowonKim <99549623+joowining@users.noreply.github.com>
2023-08-10 18:02:28 +0900

    add react-router and make path and routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import './App.css';
+import { Routes,Route } from 'react-router-dom';
 import Hompage from './components/Home.js/Hompage';
 import Explore from './components/explore/Explore';
 import NewPoster from './components/NewPoster';
 import ProfileMain from "./Homepage";
 import Reels from './components/reels/reels';
+import LoginPage from './components/LoginPage/index.js';
+import JoinPage from './components/JoinPage/index.js';
+
 
 function App() {
   return (
     <div className="App">
-      <Explore/>
-      <Hompage></Hompage>
-      <NewPoster/>
-      <ProfileMain/>
-      <Reels />
+      <Routes>
+          <Route path="/" element={<Hompage/>}/>
+          <Route path="login" element={<LoginPage/>}/>
+          <Route path="join" element={<JoinPage/>}/>
+          <Route path="profile" element={<ProfileMain/>}/>
+          <Route path="explore" element={<Explore/>}/>
+          <Route path="newposter" element={<NewPoster/>}/>
+          <Route path="reels" element={<Reels/>}/>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+/*
+<Explore/>
+      <Hompage></Hompage>
+      <NewPoster/>
+      <ProfileMain/>
+      <Reels />
+*/
