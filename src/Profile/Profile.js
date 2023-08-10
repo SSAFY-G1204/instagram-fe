97d99922897993d18916a38229d9827167c92edd
hmlee0228 <leehymy98@naver.com>
2023-08-10 16:51:55 +0900

    프로필 추가

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -2,10 +2,236 @@ import React from "react";
 import "./Profile.css";
 
 function Profile() {
-  return <div className="profile">
-    <div className="profile__top">user</div>
-    <div className="profile__bottom">user post</div>
-  </div>;
+  return (
+    <div className="profile">
+      <div className="profile__top">
+        <header className="profile__header">
+          <div className="profile__img">
+            <div type="read" scale="2.5" class="sc-gsDKAQ lddlTa">
+              <img
+                alt="chanhyuk"
+                src="https://instagram-s3-dev.s3.ap-northeast-2.amazonaws.com/member/base-UUID_base.PNG.png"
+              />
+            </div>
+          </div>
+          <section class="profile-content">
+            <div class="name-with-icon">
+              <h2 class="name">JDG</h2>
+              <a class="edit" href="/accounts/edit">
+                프로필 편집
+              </a>
+              <svg
+                aria-label="\uC635\uC158"
+                color="#262626"
+                fill="#262626"
+                height="24"
+                role="img"
+                viewBox="0 0 24 24"
+                width="24"
+              >
+                <circle
+                  cx="12"
+                  cy="12"
+                  fill="none"
+                  r="8.635"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                ></circle>
+                <path
+                  d="M14.232 3.656a1.269 1.269 0 01-.796-.66L12.93 2h-1.86l-.505.996a1.269 1.269 0 01-.796.66m-.001 16.688a1.269 1.269 0 01.796.66l.505.996h1.862l.505-.996a1.269 1.269 0 01.796-.66M3.656 9.768a1.269 1.269 0 01-.66.796L2 11.07v1.862l.996.505a1.269 1.269 0 01.66.796m16.688-.001a1.269 1.269 0 01.66-.796L22 12.93v-1.86l-.996-.505a1.269 1.269 0 01-.66-.796M7.678 4.522a1.269 1.269 0 01-1.03.096l-1.06-.348L4.27 5.587l.348 1.062a1.269 1.269 0 01-.096 1.03m11.8 11.799a1.269 1.269 0 011.03-.096l1.06.348 1.318-1.317-.348-1.062a1.269 1.269 0 01.096-1.03m-14.956.001a1.269 1.269 0 01.096 1.03l-.348 1.06 1.317 1.318 1.062-.348a1.269 1.269 0 011.03.096m11.799-11.8a1.269 1.269 0 01-.096-1.03l.348-1.06-1.317-1.318-1.062.348a1.269 1.269 0 01-1.03-.096"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                ></path>
+              </svg>
+              <svg
+                aria-label="\uC635\uC158 \uB354 \uBCF4\uAE30"
+                color="#262626"
+                fill="#262626"
+                height="24"
+                role="img"
+                viewBox="0 0 24 24"
+                width="24"
+              >
+                <circle cx="12" cy="12" r="1.5"></circle>
+                <circle cx="6.5" cy="12" r="1.5"></circle>
+                <circle cx="17.5" cy="12" r="1.5"></circle>
+              </svg>
+            </div>
+            <ul class="follower">
+              <li class="follower-with-number">
+                게시물 <span>10</span>
+              </li>
+              <li class="follower-with-number">
+                팔로워 <span>200</span>
+              </li>
+              <li class="follower-with-number">
+                팔로우 <span>100</span>
+              </li>
+            </ul>
+            <div class="detail-info">JDG-instagram clone coding</div>
+          </section>
+        </header>
+        <div class="profile__center">
+          <div class="current">
+            <svg
+              aria-label=""
+              color="#8e8e8e"
+              fill="#8e8e8e"
+              height="12"
+              role="img"
+              viewBox="0 0 24 24"
+              width="12"
+            >
+              <rect
+                fill="none"
+                height="18"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                width="18"
+                x="3"
+                y="3"
+              ></rect>
+              <line
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                x1="9.015"
+                x2="9.015"
+                y1="3"
+                y2="21"
+              ></line>
+              <line
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                x1="14.985"
+                x2="14.985"
+                y1="3"
+                y2="21"
+              ></line>
+              <line
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                x1="21"
+                x2="3"
+                y1="9.015"
+                y2="9.015"
+              ></line>
+              <line
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                x1="21"
+                x2="3"
+                y1="14.985"
+                y2="14.985"
+              ></line>
+            </svg>
+            <span>게시물</span>
+          </div>
+          <div class="">
+            <svg
+              color="#262626"
+              fill="#262626"
+              height="24"
+              role="img"
+              viewBox="0 0 24 24"
+              width="24"
+            >
+              <polygon
+                fill="none"
+                points="20 21 12 13.44 4 21 4 3 20 3 20 21"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+              ></polygon>
+            </svg>
+            <span>저장됨</span>
+          </div>
+          <div class="">
+            <svg
+              aria-label=""
+              color="#8e8e8e"
+              fill="#8e8e8e"
+              height="12"
+              role="img"
+              viewBox="0 0 24 24"
+              width="12"
+            >
+              <path
+                d="M10.201 3.797L12 1.997l1.799 1.8a1.59 1.59 0 001.124.465h5.259A1.818 1.818 0 0122 6.08v14.104a1.818 1.818 0 01-1.818 1.818H3.818A1.818 1.818 0 012 20.184V6.08a1.818 1.818 0 011.818-1.818h5.26a1.59 1.59 0 001.123-.465z"
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+              ></path>
+              <path
+                d="M18.598 22.002V21.4a3.949 3.949 0 00-3.948-3.949H9.495A3.949 3.949 0 005.546 21.4v.603"
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+              ></path>
+              <circle
+                cx="12.072"
+                cy="11.075"
+                fill="none"
+                r="3.556"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+              ></circle>
+            </svg>
+            <span>태그됨</span>
+          </div>
+        </div>
+        <div className="profile__bottom">
+          <div className="profile__post">
+            <div className="profile__post1">
+              <img
+                src="https://img.freepik.com/free-photo/cloud-and-blue-sky_1150-35749.jpg?w=996&t=st=1691651525~exp=1691652125~hmac=7be9565b0066e2524eb7f45664936c40aeb555f466f9e4625538e92eb0a3075e"
+                alt="사진"
+                className="single-content"
+              />
+              <div class="hover">
+                <ul>
+                  <li>
+                    <div width="19" height="19" class="profile__post-like"></div>
+                    <span>0</span>
+                  </li>
+                  <li>
+                    <div width="19" height="19" class="profile__post-comment"></div>
+                    <span>0</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="emptyLayout"></div>
+            <div className="emptyLayout"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
